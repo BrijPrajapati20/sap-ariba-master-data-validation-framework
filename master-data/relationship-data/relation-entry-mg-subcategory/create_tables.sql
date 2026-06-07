b66673a52678425c87bd6a88c27d78345335b75b
Brij Bhushan Prajapati <56437428+BrijPrajapati20@users.noreply.github.com>
2026-06-07 17:49:01 +0530

    Create create_tables.sql

---
+++ master-data/relationship-data/relation-entry-mg-subcategory/create_tables.sql
@@ -0,0 +1,32 @@
+/*=============================================================
+TABLE CREATION SCRIPT
+Module : Relation Entry MG Subcategory
+=============================================================*/
+
+CREATE TABLE itk_relation_entry_mg_subcategory (
+
+```
+right_key1 VARCHAR(255),
+
+left_key1 VARCHAR(255),
+
+left_id VARCHAR(255),
+
+right_id VARCHAR(255)
+```
+
+);
+
+CREATE TABLE ariba_relation_entry_mg_subcategory (
+
+```
+right_key1 VARCHAR(255),
+
+left_key1 VARCHAR(255),
+
+left_id VARCHAR(255),
+
+right_id VARCHAR(255)
+```
+
+);
