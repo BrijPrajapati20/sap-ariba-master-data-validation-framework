6992bf468bb0eb51b2561d83968bc9f72d37bf27
Brij Bhushan Prajapati <56437428+BrijPrajapati20@users.noreply.github.com>
2026-06-07 18:10:13 +0530

    Create create_tables.sql

---
+++ master-data/financial-data/asset/create_tables.sql
@@ -0,0 +1,28 @@
+/*=============================================================
+TABLE CREATION SCRIPT
+Module : Asset Validation
+=============================================================*/
+
+CREATE TABLE itk_asset (
+
+    company_code VARCHAR(20),
+
+    asset_number VARCHAR(50),
+
+    asset_description VARCHAR(255),
+
+    asset_sub_number VARCHAR(50)
+
+);
+
+CREATE TABLE ariba_asset (
+
+    company_code VARCHAR(20),
+
+    asset_number VARCHAR(50),
+
+    asset_description VARCHAR(255),
+
+    asset_sub_number VARCHAR(50)
+
+);
