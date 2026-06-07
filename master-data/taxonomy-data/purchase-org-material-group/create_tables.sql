373c853e00fa97413912b72b7d2b15e3b9b16711
Brij Bhushan Prajapati <56437428+BrijPrajapati20@users.noreply.github.com>
2026-06-07 17:37:33 +0530

    Create create_tables.sql

---
+++ master-data/taxonomy-data/purchase-org-material-group/create_tables.sql
@@ -0,0 +1,20 @@
+/*=========================================================
+TABLE CREATION
+Module : Purchase Org Material Group
+=========================================================*/
+
+CREATE TABLE itk_purchase_org_material_group (
+
+```
+uniq_name VARCHAR(200)
+```
+
+);
+
+CREATE TABLE ariba_purchase_org_material_group (
+
+```
+unique_name VARCHAR(200)
+```
+
+);
