dc7120f14b8a11f4e61a4b70b8e655c6e40853ab
Brij Bhushan Prajapati <56437428+BrijPrajapati20@users.noreply.github.com>
2026-06-07 18:27:33 +0530

    Create create_tables.sql

---
+++ master-data/finance-governance/account-category/create_tables.sql
@@ -0,0 +1,20 @@
+/*=============================================================
+TABLE CREATION SCRIPT
+Module : Account Category Validation
+=============================================================*/
+
+CREATE TABLE itk_account_category (
+
+    account_category_code VARCHAR(20),
+
+    account_category_description VARCHAR(255)
+
+);
+
+CREATE TABLE ariba_account_category (
+
+    account_category_code VARCHAR(20),
+
+    account_category_description VARCHAR(255)
+
+);
