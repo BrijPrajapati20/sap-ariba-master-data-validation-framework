804c5548329be2a0cf38be85c55bb5da17c90900
Brij Bhushan Prajapati <56437428+BrijPrajapati20@users.noreply.github.com>
2026-06-07 17:28:10 +0530

    Create create_tables.sql

---
+++ master-data/taxonomy-data/material-group-subcategory/create_tables.sql
@@ -0,0 +1,20 @@
+/*=========================================================
+TABLE CREATION
+Module : Material Group Subcategory
+=========================================================*/
+
+CREATE TABLE itk_material_group_subcategory (
+
+    mtgrp_subc VARCHAR(50),
+
+    mtgrp_subcd VARCHAR(255)
+
+);
+
+CREATE TABLE ariba_material_group_subcategory (
+
+    subcategory_code VARCHAR(50),
+
+    subcategory_description VARCHAR(255)
+
+);
