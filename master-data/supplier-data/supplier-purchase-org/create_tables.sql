611680385625f866cc4dee8ca88d672ebaca8854
Brij Bhushan Prajapati <56437428+BrijPrajapati20@users.noreply.github.com>
2026-06-07 18:06:59 +0530

    Create create_tables.sql

---
+++ master-data/supplier-data/supplier-purchase-org/create_tables.sql
@@ -0,0 +1,24 @@
+/*=============================================================
+TABLE CREATION SCRIPT
+Module : Supplier Purchase Organization Validation
+=============================================================*/
+
+CREATE TABLE itk_supplier_purchase_org (
+
+    vendor_id VARCHAR(100),
+
+    purchase_org VARCHAR(50),
+
+    payment_term VARCHAR(50)
+
+);
+
+CREATE TABLE ariba_supplier_purchase_org (
+
+    vendor_id VARCHAR(100),
+
+    purchase_org VARCHAR(50),
+
+    payment_term VARCHAR(50)
+
+);
